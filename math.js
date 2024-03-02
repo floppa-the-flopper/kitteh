195424af2a4781af213d08246fca7f2297696428
floppa-the-flopper <142877833+floppa-the-flopper@users.noreply.github.com>
2024-03-02 13:50:21 +1000

    Update math.js

--- math.js
+++ math.js
@@ -16,6 +16,12 @@ function openWindow(url) {
 	window.open(url, "_blank", 'menubar=no, status=no, toolbar=no, resizable=no, width=357, height=330, titlebar=no, alwaysRaised=yes');
 }
 
+function openWindowAmountOfTimes(url,times) {
+	for (let i = 0; i < times; i++) {
+		openWindow(url)
+	}
+}
+
 function annoyments() {
     xPos = randomRange(screen.width + 357, screen.width - 357);
     yPos = randomRange(screen.height + 330, screen.height - 330);
