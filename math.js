9e0b947cc2d8a9aa8a5f8a31fdd413da7da6e18b
floppa-the-flopper <142877833+floppa-the-flopper@users.noreply.github.com>
2024-03-02 12:41:02 +1000

    Create math.js

---
+++ math.js
@@ -0,0 +1,24 @@
+let xPos = 400;
+let yPos = -100;
+
+function randomRange(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+  
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function changeTitle(title) {
+	document.title = title;
+}
+
+function openWindow(url) {
+	window.open(url, "_blank", 'menubar=no, status=no, toolbar=no, resizable=no, width=357, height=330, titlebar=no, alwaysRaised=yes');
+}
+
+function annoyments() {
+    xPos = randomRange(screen.width + 357, screen.width - 357);
+    yPos = randomRange(screen.height + 330, screen.height - 330);
+    window.moveTo(xPos, yPos);
+    setTimeout(annoyments, 1);
+}
